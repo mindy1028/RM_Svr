cab69ef669a900399bffc286c6cc271d15f408c8
15810559059@163.com <15810559059@163.com>
2022-02-23 22:58:59 +0800

    Create the software ReadMe file

--- RM_Svr/RM_Svr_Arch.h
+++ RM_Svr/RM_Svr_Arch.h
@@ -1,11 +1,27 @@
 /*************************************************
 
-Copyright:¹ù²ÅÈÃµ±ÖÇe
+Copyright:Lemon Mindy
 
 Author:Mindy
 
-Date:2022-2-22
+Date:2022-2-23
 
 Description:RM_Svr Architecture By C++
+
+**************************************************/
+
+
+/*************************************************
+
+The software architecture realizes database API encapsulation, communication(Tcp, Http, Udp)
+and other extensible module combinations, of course, including separate log module encapsulation;
+All modules can be loaded through the master module(master) selection, and provide external
+interface for UI call, the software not only includes the underlying architecture but also
+	includes the external support interface to the three - party framework such as Cocox;
+From the language point of view, the software provides an interface to support pythonand PHP.
+Most importantly, it will fulfill my current dream of creating a highly extensible Web server that supports
+dynamic data changesand server - side Bridges for a wide variety of clients.
+
+																		¡ª¡ª¡ª¡ªCreated When 2022-2-23
 
 **************************************************/
