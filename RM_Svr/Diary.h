edc662cb8fee34d8bca726a44153ce8684a8a19f
15810559059@163.com <15810559059@163.com>
2022-02-24 23:33:22 +0800

    Create Diary File for Mindy's Architecture.

---
+++ RM_Svr/Diary.h
@@ -0,0 +1,20 @@
+/*************************************************
+
+Copyright:Lemon Mindy
+
+Author:Mindy
+
+Date:2022-2-23
+
+Description:RM_Svr Development diary By C++
+
+**************************************************/
+
+/*************************************************
+		*    Development diary Begin    *
+		* 
+		*		Date:2022-2-24 Wuhan
+		*		Create Diary File for Mindy's Architecture.
+		* 
+		*    Development diary  End     *
+**************************************************/
